d4c7cd5593bf6bf5199dff6d89d801b81fa29876
vortex <sobrinhosj@gmail.com>
2026-09-10 20:10:12 -0300

    feat(separation): add source separation package

---
+++ packages/separation/__init__.py
@@ -0,0 +1,5 @@
+"""Source separation services for MELO."""
+
+from packages.separation.demucs import SeparationResult, separate_vocals_instrumental
+
+__all__ = ["SeparationResult", "separate_vocals_instrumental"]
